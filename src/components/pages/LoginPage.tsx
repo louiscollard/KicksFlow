8054480608cde1d0e1a1a22ba4ee6f8a84d998cb
Ton Nom <louiscollard2@gmail.com>
2026-06-06 09:02:07 +0200

    F01: ✨ implement LoginPage form

--- src/components/pages/LoginPage.tsx
+++ src/components/pages/LoginPage.tsx
@@ -1,4 +1,15 @@
+import { Button } from "@/components/ui/Button";
+import { Input } from "@/components/ui/Input";
+import { Label } from "@/components/ui/Label";
+import { useState } from "react";
+
 export function LoginPage() {
+    const [firstName , setFirstName ] = useState("");
+
+    const handleSubmit = (e: React.SyntheticEvent<HTMLFormElement>) => {
+        e.preventDefault()
+    }
+
     return (
         <div className="min-h-screen w-screen flex items-center justify-center bg-gray-50/50 px-4 antialiased">
             <div className="w-full max-w-md bg-white rounded-2xl p-8 shadow-xs border border-gray-100 flex flex-col gap-8">
@@ -15,22 +26,24 @@ export function LoginPage() {
                         <div className="h-px w-8 bg-gray-200"></div>
                     </div>
                 </div>
-
-                <div className="flex flex-col gap-4">
+                <form onSubmit={handleSubmit} className="flex flex-col gap-4">
                     <div className="flex flex-col gap-1.5">
-                        <label className="text-xs font-bold text-gray-700 uppercase tracking-wide">
-                        Entrez votre prénom :
-                        </label>
-                        <input 
-                        type="text" 
-                        placeholder="Ex: Louis"
-                        className="w-full rounded-xl border border-gray-200 bg-gray-50/50 px-4 py-3 text-sm text-gray-900 placeholder-gray-400 outline-hidden transition-all duration-200 hover:border-gray-300 focus:border-primary focus:bg-white focus:ring-2 focus:ring-primary/10"
+                        <Label htmlFor="firstName" className="text-xs font-bold text-gray-700 uppercase tracking-wide">
+                            Entrez votre prénom :
+                        </Label>
+                        <Input
+                            id="firstName"
+                            name="firstName"
+                            type="text"
+                            placeholder="Ex: Louis"
+                            value={firstName}
+                            onChange={(e) => setFirstName(e.target.value)}
                         />
                     </div>
-                    <button className="w-full mt-2 rounded-xl bg-primary px-4 py-3.5 text-sm font-bold text-white shadow-sm transition-all duration-200 hover:bg-primary/90 hover:shadow-md active:scale-[0.99] cursor-pointer">
+                    <Button type="submit" className="w-full mt-2">
                         Accéder à votre espace
-                    </button>
-                </div>
+                    </Button>
+                </form>
             </div>
         </div>
     );
